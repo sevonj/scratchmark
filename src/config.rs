eec3c5df5bc54e7b6530c011300b25df486ae604
sevonj <100710152+sevonj@users.noreply.github.com>
2026-01-18 12:38:34 +0200

    chore: v1.7.1

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,4 @@
-pub static VERSION: &str = "1.7.0";
+pub static VERSION: &str = "1.7.1";
 pub static GETTEXT_PACKAGE: &str = "scratchmark";
 pub static LOCALEDIR: &str = "/usr/local/share/locale";
 pub static PKGDATADIR: &str = "/usr/local/share/scratchmark";
