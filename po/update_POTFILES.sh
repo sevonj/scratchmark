c781e0cd542c16ddc227611856c2cc5dd01445b1
sevonj <100710152+sevonj@users.noreply.github.com>
2026-02-04 20:34:13 +0200

    ci: verify potfiles rewrite in python

--- po/update_POTFILES.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-# Run from repo root.
-# Auto crawl every ui file into POTFILES.in
-
-rm -f po/POTFILES.in
-cp po/POTFILES.in.in po/POTFILES.in
-find data/resources/ui -type f -name \*.ui -printf '%h\0%p\n' | sort | awk -F '\0' '{print $2}' >> po/POTFILES.in
--- po/verify_POTFILES.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-# Run from repo root.
-# CI check to make sure POTFILES.in is correct
-
-cp po/POTFILES.in.in po/POTFILES.in.temp
-find data/resources/ui -type f -name \*.ui -printf '%h\0%p\n' | sort | awk -F '\0' '{print $2}' >> po/POTFILES.in.temp
-diff ./po/POTFILES.in ./po/POTFILES.in.temp
