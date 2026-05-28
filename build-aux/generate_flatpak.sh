201746d08d3a185544751d6d56128f970769da77
sevonj <100710152+sevonj@users.noreply.github.com>
2026-05-28 12:52:14 +0300

    misc: tweak flatpak script

--- build-aux/generate_flatpak.sh
+++ build-aux/generate_flatpak.sh
@@ -5,5 +5,5 @@ set -e
 # curl -o flatpak-cargo-generator.py https://raw.githubusercontent.com/flatpak/flatpak-builder-tools/refs/heads/master/cargo/flatpak-cargo-generator.py
 # python3 flatpak-cargo-generator.py ../Cargo.lock -o generated-sources.json
 
-flatpak-builder --repo=flatpakrepo flatpak org.scratchmark.Scratchmark.yml --force-clean
+flatpak run org.flatpak.Builder --repo=flatpakrepo flatpak org.scratchmark.Scratchmark.yml --force-clean
 flatpak build-bundle flatpakrepo/ Scratchmark.flatpak org.scratchmark.Scratchmark
